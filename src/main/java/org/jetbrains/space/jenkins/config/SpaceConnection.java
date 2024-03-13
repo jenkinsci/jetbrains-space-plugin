53748245964493d1d571f9fedcb8d857f97b1234
Kiryushin-Andrey <kiryushin.andrey@gmail.com>
2024-03-13 12:35:15 +0100

    Clean up permission checks

--- src/main/java/org/jetbrains/space/jenkins/config/SpaceConnection.java
+++ src/main/java/org/jetbrains/space/jenkins/config/SpaceConnection.java
@@ -2,6 +2,7 @@ package org.jetbrains.space.jenkins.config;
 
 import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey;
 import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
 import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
 import com.cloudbees.plugins.credentials.domains.URIRequirementBuilder;
 import hudson.Extension;
@@ -12,14 +13,14 @@ import hudson.security.ACL;
 import hudson.util.FormValidation;
 import hudson.util.ListBoxModel;
 import jenkins.model.Jenkins;
+import org.apache.commons.lang.StringUtils;
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.verb.POST;
 
 import javax.annotation.Nullable;
 import java.util.UUID;
-
-import static org.apache.commons.lang.StringUtils.isBlank;
 
 /**
  * Represents a connection to JetBrains Space instance, with its base url and both API and SSH credentials.
@@ -34,7 +35,7 @@ public class SpaceConnection extends AbstractDescribableImpl<SpaceConnection> {
 
     @DataBoundConstructor
     public SpaceConnection(@Nullable String id, String name, String baseUrl, String apiCredentialId, String sshCredentialId) {
-        this.id = isBlank(id) ? UUID.randomUUID().toString() : id;
+        this.id = StringUtils.isBlank(id) ? UUID.randomUUID().toString() : id;
         this.name = name;
         this.baseUrl = baseUrl;
         this.apiCredentialId = apiCredentialId;
@@ -66,8 +67,14 @@ public class SpaceConnection extends AbstractDescribableImpl<SpaceConnection> {
     public static class DescriptorImpl extends Descriptor<SpaceConnection> {
 
         @POST
-        public ListBoxModel doFillApiCredentialIdItems(@QueryParameter String baseUrl) {
-            Jenkins.get().checkPermission(Item.CONFIGURE);
+        public ListBoxModel doFillApiCredentialIdItems(@AncestorInPath Item item, @QueryParameter String baseUrl, @QueryParameter String apiCredentialId) {
+            if (item == null && !Jenkins.get().hasPermission(Jenkins.ADMINISTER)) {
+                return new StandardListBoxModel().includeCurrentValue(apiCredentialId);
+            }
+            if (item != null && !item.hasPermission(Item.EXTENDED_READ) && !item.hasPermission(CredentialsProvider.USE_ITEM)) {
+                return new StandardListBoxModel().includeCurrentValue(apiCredentialId);
+            }
+
             return new StandardListBoxModel()
                     .includeMatchingAs(
                             ACL.SYSTEM2,
@@ -79,8 +86,14 @@ public class SpaceConnection extends AbstractDescribableImpl<SpaceConnection> {
         }
 
         @POST
-        public ListBoxModel doFillSshCredentialIdItems(@QueryParameter String baseUrl) {
-            Jenkins.get().checkPermission(Item.CONFIGURE);
+        public ListBoxModel doFillSshCredentialIdItems(@AncestorInPath Item item, @QueryParameter String baseUrl, @QueryParameter String sshCredentialId) {
+            if (item == null && !Jenkins.get().hasPermission(Jenkins.ADMINISTER)) {
+                return new StandardListBoxModel().includeCurrentValue(sshCredentialId);
+            }
+            if (item != null && !item.hasPermission(Item.EXTENDED_READ) && !item.hasPermission(CredentialsProvider.USE_ITEM)) {
+                return new StandardListBoxModel().includeCurrentValue(sshCredentialId);
+            }
+
             return new StandardListBoxModel()
                     .includeMatchingAs(
                             ACL.SYSTEM2,
@@ -93,14 +106,13 @@ public class SpaceConnection extends AbstractDescribableImpl<SpaceConnection> {
 
         @POST
         public FormValidation doTestApiConnection(@QueryParameter String baseUrl, @QueryParameter String apiCredentialId) {
-            Jenkins.get().checkPermission(Item.CONFIGURE);
             try {
                 UtilsKt.testSpaceApiConnection(baseUrl, apiCredentialId);
-            } catch (RuntimeException ex) {
+            } catch (Exception ex) {
                 return FormValidation.error(ex, "Couldn't connect to JetBrains Space API");
             }
 
-            return FormValidation.ok();
+            return FormValidation.ok("Connection established successfully");
         }
     }
 }
--- src/main/java/org/jetbrains/space/jenkins/scm/SpaceSCM.java
+++ src/main/java/org/jetbrains/space/jenkins/scm/SpaceSCM.java
@@ -36,8 +36,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-import static org.jetbrains.space.jenkins.config.UtilsKt.checkPermissions;
-
 /**
  * <p>{@link SCM} implementation for JetBrains Space. Wraps the standard Git SCM implementation, adding some features on top of it.</p>
  * <p>Allows checking out source code by selecting a configured Space connection and then choosing a project and repository,
